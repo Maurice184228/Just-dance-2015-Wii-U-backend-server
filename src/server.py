dffda69122ec72511b0adee77c9ce6053ce292a1
Maurice P. <mpinard2006@gmail.com>
2026-09-17 23:25:38 -0400

    added gateawayressources array obj

--- src/server.py
+++ src/server.py
@@ -326,9 +326,28 @@ async def application_configuration(
         },
         "legacyUrls": {},
         "featuresSwitches": {
-            "Connection": True,
+             "name": "Connection",
+             "value": True,
             },
+        "gatewayResources": [
+    {
+        "url": "https://api-ubiservices.ubi.com/{version}/profiles/connections",
+        "name": "all_connections",
+        "version": 1,
+    },
+    {
+        "url": "https://api-ubiservices.ubi.com/{version}/profiles/{profileId}/connections",
+        "name": "connections",
+        "version": 1,
+    },
+    {
+        "url": "wss://api-ubiservices.ubi.com/{version}/websocket",
+        "name": "websocket/server",
+        "version": 2,
+    },
+],
     }
+    
 
     print("[JobRequestConfig] Returning development configuration:")
     print(response)
