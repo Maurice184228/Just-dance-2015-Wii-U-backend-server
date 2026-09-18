e98e1f2c78bbd92ad3ca2a97424b6aeee30f0e71
Maurice P. <mpinard2006@gmail.com>
2026-09-17 23:55:11 -0400

    fixed an issue of crash

--- src/ubiservices/session_info.py
+++ src/ubiservices/session_info.py
@@ -23,22 +23,22 @@ class SessionInfo:
     initialize_user: bool
     platform_type: str
 
-def to_dict(self) -> dict[str, Any]:
-    return {
-        "token": self.token,
-        "ticket": self.ticket,
-        "expiration": self.expiration,
-        "platformType": self.platform_type,
-        "profileId": self.profile_id,
-        "productId": self.product_id,
-        "userId": self.user_id,
-        "nameOnPlatform": self.name_on_platform,
-        "initializeUser": self.initialize_user,
-        "spaceId": self.space_id,
-        "environment": self.environment,
-        "hasAcceptedLegalOptins": self.has_accepted_legal_optins,
-        "accountIssues": self.account_issues,
-        "sessionId": self.session_id,
-        "clientIp": self.client_ip,
-        "serverTime": self.server_time,
-    }
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "token": self.token,
+            "ticket": self.ticket,
+            "expiration": self.expiration,
+            "platformType": self.platform_type,
+            "profileId": self.profile_id,
+            "productId": self.product_id,
+            "userId": self.user_id,
+            "nameOnPlatform": self.name_on_platform,
+            "initializeUser": self.initialize_user,
+            "spaceId": self.space_id,
+            "environment": self.environment,
+            "hasAcceptedLegalOptins": self.has_accepted_legal_optins,
+            "accountIssues": self.account_issues,
+            "sessionId": self.session_id,
+            "clientIp": self.client_ip,
+            "serverTime": self.server_time,
+        }
