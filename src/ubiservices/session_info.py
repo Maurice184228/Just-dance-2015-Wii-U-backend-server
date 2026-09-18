979af5379d93497004be4cc01d755c162ce16417
Maurice P. <mpinard2006@gmail.com>
2026-09-17 23:50:11 -0400

    Added product_id info in session_info/service

--- src/ubiservices/session_info.py
+++ src/ubiservices/session_info.py
@@ -8,6 +8,7 @@ from typing import Any
 class SessionInfo:
     session_id: str
     profile_id: str
+    product_id: str
     user_id: str
     space_id: str
     environment: str
@@ -22,21 +23,22 @@ class SessionInfo:
     initialize_user: bool
     platform_type: str
 
-    def to_dict(self) -> dict[str, Any]:
-        return {
-            "token": self.token,
-            "ticket": self.ticket,
-            "expiration": self.expiration,
-            "platformType": self.platform_type,
-            "profileId": self.profile_id,
-            "userId": self.user_id,
-            "nameOnPlatform": self.name_on_platform,
-            "initializeUser": self.initialize_user,
-            "spaceId": self.space_id,
-            "environment": self.environment,
-            "hasAcceptedLegalOptins": self.has_accepted_legal_optins,
-            "accountIssues": self.account_issues,
-            "sessionId": self.session_id,
-            "clientIp": self.client_ip,
-            "serverTime": self.server_time,
-        }
+def to_dict(self) -> dict[str, Any]:
+    return {
+        "token": self.token,
+        "ticket": self.ticket,
+        "expiration": self.expiration,
+        "platformType": self.platform_type,
+        "profileId": self.profile_id,
+        "productId": self.product_id,
+        "userId": self.user_id,
+        "nameOnPlatform": self.name_on_platform,
+        "initializeUser": self.initialize_user,
+        "spaceId": self.space_id,
+        "environment": self.environment,
+        "hasAcceptedLegalOptins": self.has_accepted_legal_optins,
+        "accountIssues": self.account_issues,
+        "sessionId": self.session_id,
+        "clientIp": self.client_ip,
+        "serverTime": self.server_time,
+    }
